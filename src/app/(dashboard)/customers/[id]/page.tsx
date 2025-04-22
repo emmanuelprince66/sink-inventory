dc4ba0640a2b9b1c17306d1fb6150256126f4b4b
Emmanuel <emmanuelochigbo60@gmail.com>
2025-04-22 14:19:21 -0400

    new ccc

--- src/app/(dashboard)/customers/[id]/page.tsx
+++ src/app/(dashboard)/customers/[id]/page.tsx
@@ -1,12 +1,12 @@
+// src/app/(dashboard)/customers/[id]/page.tsx
 import React from "react";
 import Contact from "./Contact";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
+interface Props {
+  params: { id: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
-export default function Page({ params }: PageProps) {
+export default function CustomerPage({ params }: Props) {
   return <Contact id={params.id} />;
 }
