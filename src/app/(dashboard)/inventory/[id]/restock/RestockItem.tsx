95bb0c6905b46f96ad31d1001123ce2577055979
Emmanuel Ochigbo <emmanuelochigbo60@gmail.com>
2025-07-18 05:08:15 -0400

    some good changes

--- src/app/(dashboard)/inventory/[id]/restock/RestockItem.tsx
+++ src/app/(dashboard)/inventory/[id]/restock/RestockItem.tsx
@@ -74,7 +74,6 @@ const RestockItem = ({
                 <FormLabel>Stock Quantity</FormLabel>
                 <FormControl>
                   <Input
-                    disabled
                     type="number"
                     placeholder="Enter Stock Quantity...."
                     {...field}
