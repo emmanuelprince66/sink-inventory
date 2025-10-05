e6c3df352209895026ee232d454fbe9a9f7e259f
Emmanuel Ochigbo <emmanuelochigbo60@gmail.com>
2025-10-05 02:26:29 -0400

    pushed to business

--- src/constants/base-url.ts
+++ src/constants/base-url.ts
@@ -1,2 +1,2 @@
-export const BaseUrl = "https://staging-api.sync360.africa/api/v1/";
-// export const BaseUrl = "https://api.sync360.africa/api/v1/";
+// export const BaseUrl = "https://staging-api.sync360.africa/api/v1/";
+export const BaseUrl = "https://api.sync360.africa/api/v1/";
