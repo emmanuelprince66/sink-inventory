bf14352e5903a5a5a69befd10ae4bd580a28e896
Emmanuel Ochigbo <emmanuelochigbo60@gmail.com>
2025-11-11 10:24:42 +0100

    new dflos

--- src/hooks/useStoreHook.tsx
+++ src/hooks/useStoreHook.tsx
@@ -62,7 +62,7 @@ export const useStoreHook = () => {
   // Initialize store data from API
   useEffect(() => {
     if (findBusiness) {
-      const baseUrl = "https://store.sync360.africa/in-store";
+      const baseUrl = "https://store.sync360.africa";
       const slugUrl = findBusiness.store_url || "";
       const outStoreUrl = `${baseUrl}/out-store/?slug=${slugUrl}`;
       const inStoreUrl = `${baseUrl}/in-store/?slug=${slugUrl}`;
